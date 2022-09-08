db4a249b5e8ad739ba0a428b48d25f1f35320428
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2022-09-08 23:30:53 +0200

    Create temp.py
    Kod som skriver data från Temp sensor och arduino i en CSV fil som sedan läses av webbläsaren

---
+++ temp.py
@@ -0,0 +1,22 @@
+import time
+import serial
+from w1thermsensor import W1ThermSensor
+sensor = W1ThermSensor()
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
+
+while True:
+ line = ser.readline().decode('utf-8').rstrip()
+
+ temperature = sensor.get_temperature()
+
+    #Write data to file in CSV format
+ with open ('/var/www/html/temp.csv','a') as datafile:
+  datafile.write(str(sensor.get_temperature()) + ("\n"))
+ time.sleep(1)
+
+ with open ('/var/www/html/arduino.csv','a') as datafile:
+  datafile.write(str(line) + ("\n"))
+ time.sleep(1)
+
+
