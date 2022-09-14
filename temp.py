94d21b1117ac988bbeb6bed678fb51290ab964b3
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2022-09-14 15:11:58 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,22 +2,14 @@ import time
 import serial
 from w1thermsensor import W1ThermSensor
 sensor = W1ThermSensor()
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
 while True:
- line = ser.readline().decode('utf-8').rstrip()
-
- temperature = sensor.get_temperature()
+ temperature = str(sensor.get_temperature())
+ siffror = temperature[0:3]
 
     #Write data to file in CSV format
  with open ('/var/www/html/temp.csv','a') as datafile:
-  datafile.write(str(sensor.get_temperature()) + ("\n"))
+  datafile.write(str(siffror) + ("\n"))
  time.sleep(1)
-
- with open ('/var/www/html/arduino.csv','a') as datafile:
-  datafile.write(str(line) + ("\n"))
- time.sleep(1)
- 
-
 
