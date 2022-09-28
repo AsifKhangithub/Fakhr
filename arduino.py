184fd18b0f3f493b293880bc059583e0dc08a6c9
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2022-09-28 12:11:00 +0200

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -1,12 +1,15 @@
+    #Vi importerar Seriel bibloteket för läsning av serial data från arduino
 import serial
+    #Datan från arduino hamna i /dev/ttyACM0 och det kommer med 9600 bithastighet. vi döper den till ser.
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
 while True:
+     #När vi kör koden då läser vi datan som kommer in.
  line = ser.readline().decode('utf-8').rstrip()
 
-    #Write data to file in CSV format
-
+    #Skickar datan till arduino.csv som läses senare av webbsidan
+    #Efter det lägger vi till ny rads tecken för att skriva det nya data i en ny rad.
  with open ('/var/www/html/arduino.csv','a') as datafile:
   datafile.write(str(line) + ("\n"))
 
