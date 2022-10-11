2678bce9eeb2924b1bb074c9d57ea52a2257cca7
husseinr@Dservices <john.hu.rob@gmail.com>
2022-10-11 16:33:51 +0200

    Hussein

--- arduino.py
+++ arduino.py
@@ -1,26 +1,26 @@
 
-import serial # importerar serial library där vi kan arbeta med  seriell kommunikation
+import serial # Importerar serial library för att kunna arbeta med seriell kommunikation
 
-# variabel ser skaffas för den seriella anslutningen för att lättare att referas till den
-# Den första är ansltningen via USB, data från arduino hamnar, bit hastighet sätts till 9600 sist en timeout en sekund
+# Variabeln 'ser' skaffas för den seriella anslutningen. Detta för att lättare att referas till den.
+# Den första är ansltningen via USB, data från arduino hamnar, bit hastighet sätts till 9600 sist en timeout en sekund.
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-# Spola bufferten, rensar platsen där allt info kommer in innan det sätts igång 
+# Spola bufferten, rensar platsen där all info kommer in innan det sätts igång.
 ser.reset_input_buffer()
 
 
-while True: # En kontitinerlig loop
+while True: # En kontinuerlig loop
 # Readline läser in det som kommer från serilaanslutningen. UTF8 Decoder är en teckenavkodning som kan göra alla Unicode-tecken läsbara.
-#Slutligen avslutas med rstrip() tar bort alla efterföjare slutet av strängen mellanslag som standard. 
+#Slutligen avslutas med rstrip() tar bort alla efterföjare slutet av strängen mellanslag som standard.
 
  line = ser.readline().decode('utf-8').rstrip()
-     # När vi läser data från arduino och vind sensor står stilla
-     # Då får vi - värden och för att få bort det använde vi replace
+     # När data läses från arduino och vind sensor står stilla
+     # Då får man minustecken (-) med i värden. För att få bort det användes replace.
  tabort_minus = line.replace('-', '0.00')
-    # Och här läser vi första 4 värden som kommer från sensorn.
+    # Och här läses de första 4 värden som kommer från sensorn.
  siffror = tabort_minus[0:4]
 
     #Skickar datan till arduino.csv som läses senare av webbsidan
-    #Efter det lägger vi till ny rads tecken för att skriva det nya data i en ny rad.
+    #Efter det lägges till en ny rads tecken för att skriva det nya data i en ny rad.
  with open ('/var/www/html/arduino.csv','a') as datafile:
   datafile.write(str(line) + ("\n"))
--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-    #Vi importerar bibloteket för tid och tempsensor data från sensor Biblotek för temp sensor behvöer installeras med <sudo apt-get install python3-w1thermsensor>
+    #Det importerar bibloteket för tid och tempsensor data från sensor Biblotek för temp sensor behvöer installeras med <sudo apt-get install python3-w1thermsensor>
      # Eller installera via < sudo pip3 install w1thermsensor > innan installering måste man aktivera 1-wire Interface genom att skriva i kommando raden <sudo raspi-config>
 import time
 from w1thermsensor import W1ThermSensor
@@ -6,7 +6,7 @@ sensor = W1ThermSensor()
 
 
 while True:
- #Vi läser data när koden körs från sensorn.
+ #Här läses data när koden körs från sensorn.
  temperature = str(sensor.get_temperature())
  # Här kontrollerar vi att vi får in data men inte alla siffror som kommer in eftersom vi får många decimaler
  # Då gör vi index 0 till 4 för att läsa första 5 siffror inklusive '.' så det blir ex "23.00" .
